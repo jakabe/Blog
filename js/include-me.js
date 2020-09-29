ad3c05245ce1205fe5334f5558528c66ea2c32a3
jakabe <31673937+jakabe@users.noreply.github.com>
2020-09-29 09:27:34 -0500

    Added Files for Repo

---
+++ js/include-me.js
@@ -0,0 +1,44 @@
+
+function includeHTML() 
+{
+    console.log(`includeHTML Called `);
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      /*search for elements with a certain atrribute:*/
+      file = elmnt.getAttribute("w3-include-html");
+
+      if (file) {
+        console.log(file);
+        /* Make an HTTP request using the attribute value as the file name: */
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200 ) {
+              
+              elmnt.innerHTML = this.responseText;
+
+            }
+            if (this.status == 404) {
+              elmnt.innerHTML = "Page not found.";
+            }
+            if(this.status == 0){
+              console.log(`status is 0! file not from webserver`);
+              console.log(this.responseText);
+            }
+            console.log(this.status);
+            /* Remove the attribute, and call this function once more: */
+            elmnt.removeAttribute("w3-include-html");
+            includeHTML();
+          }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /* Exit the function: */
+        console.log(`includeHTML about to terminate`);
+        return;
+      }
+    }
+}
