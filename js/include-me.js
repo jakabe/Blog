d4a55db052287211ffb49e5db51329530b4abb19
jakabe <jacobcgwenger93@gmail.com>
2020-10-27 12:26:21 -0500

    made blog post for today
    new blog post

--- js/include-me.js
+++ js/include-me.js
@@ -1,12 +1,50 @@
+/**
+flags the time with a string
+*/
+function f(strang){
+    d=new Date();
+    console.log(`   ${strang} @${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}:${d.getMilliseconds()}`);
+}
 
-function includeHTML() 
+/**
+    easier print function
+*/
+function p(pr){
+    console.log(pr);
+}
+
+function clf(s)
 {
-    //console.log(`includeHTML Called `);
+    f(`[I!] `+s);
+}
+
+function includeHTML(rv) 
+{
+    var recursionVar = 0;
+    if(rv == 0)
+    {
+
+    } else 
+    {
+      recursionVar = rv;
+    }
+
+    clf('includeHTML('+recursionVar+')');
     var z, i, elmnt, file, xhttp;
     /* Loop through a collection of all HTML elements: */
     z = document.getElementsByTagName("*");
     for (i = 0; i < z.length; i++) {
       elmnt = z[i];
+
+      //build debug string
+      // var debugString ="Layer ";
+      // for(var spaces = 0; spaces < recursionVar; spaces++)
+      // {
+      //   debugString+="-> ";
+      // }
+
+      // //
+      // f(`${debugString}[I!] checking ${z[i]}`);
       /*search for elements with a certain atrribute:*/
       file = elmnt.getAttribute("w3-include-html");
 
@@ -31,7 +69,7 @@ function includeHTML()
             //console.log(this.status);
             /* Remove the attribute, and call this function once more: */
             elmnt.removeAttribute("w3-include-html");
-            includeHTML();
+            includeHTML(recursionVar+1);
           }
         }
         xhttp.open("GET", file, true);
