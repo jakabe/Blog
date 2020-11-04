51ecd169405aaa4a16785c81cf5dab29b78652e6
jakabe <jacobcgwenger93@gmail.com>
2020-11-04 14:29:14 -0600

    Updated makePost.java
    I forgot some criteria yesterday.  Now it's much prettier.

--- makePost.java
+++ makePost.java
@@ -2,128 +2,206 @@
 import java.util.*; 
 import java.io.FileWriter;   // Import the FileWriter class
 import java.util.Scanner;
-
+import java.nio.file.Files;
 
 import java.io.File;  // Import the File class
 import java.io.IOException;  // Import the IOException class to handle errors
 
+//things have changed since my day
+import java.util.logging.Logger;
 
+//gotta use some regex here
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+/****************************************************************************
+ * App Requirements
+ * 
+ * 
+ *    UI
+      accept directory for folder/files [x]
+      accepts date as folder name [x]
+      accepts blog or microblog as arguments [x]
+      provides directory contents of folder [x]
+      prompt user to review changes [x]
+
+      Functions
+      parse keyboard input for correct formatting [x]
+      check if dated folder exists [x]
+      check if dated html file exits (blog or microblog) [x]
+      create new folder for indicated date [x]
+      create new html file for indicated date [x]
+      populate html file with boilerplate html code  [x]
+      correctly adjust date of html post with date provided by user input [x]
+****************************************************************************/
+//2020-11-04@08:30
 public class makePost {
-
-   /* This is my first java program.
-    * This will print 'Hello World' as the output
-    */
-
-   public static void main(String []args) {
-      System.out.println("\n Hello World \n"); // prints Hello World
-
-      Scanner keyboard = new Scanner(System.in);
-      
-      
-      String newFolderName = getFolderName();
-
-      System.out.println("The new folder name is "+newFolderName);
-      
+   public static void main(String []args) throws IOException {
+      Scanner kbd = new Scanner(System.in);
+      //get new foldername from getFolderName method
+      String newFolderName = getFolderName(); 
+      //get new file name from getFileName method
       String newFileName = getFileName();
-      
-      System.out.println("Folder ["+newFolderName+"]/["+newFileName+"] will now be created");
-      
+      //make the desired directory 
       String newDir = makeDirectoryWithFolderName(newFolderName);
-      String file_and_Dir = makeFileWithFileName(newFileName,newDir);
-     
-      writeToTheFile(file_and_Dir, newFileName);
-
-      //UNIT TESTS
-      //getFolderNameTESTMethod();
-
-
-
+      //make the file in the directory
+      String fileWithDir = makeFileWithFileName(newFileName,newDir); //
+      //now that we have forced the filename to be appropriate we may write to file
+      writeToTheFile(fileWithDir, newFileName);
+      //close the keyboard scanner
+      kbd.close();
    }
 
-   /**
-    * 
+   
 
-    */
-   public static void writeToTheFile(String file_and_Directory, String file_name){
-      try {
-         FileWriter myWriter = new FileWriter(file_and_Directory);
-         //we need some html script in a string
-         String newHtmlString="";
-         newHtmlString =
+   
+   public static void writeToTheFile(String fileAndDirectoryString, String fileNameString){
+      Logger methodLogger = Logger.getLogger(makePost.class.getName()+"-m1");
+      String largeHtmlString=
          "<link rel=\"stylesheet\" media=\"screen\" href=\"https://fontlibrary.org/face/monoid\" type=\"text/css\"/>"+
          "\n<div class=\"filterDiv show\">"+
          "\n\t<article class=\"l\">" +
-         "\n\t\t<a class=\"date\" date=\""+file_name+"\" id=\""+file_name+"\">"+file_name+"</a>"+
+         "\n\t\t<a class=\"date\" date=\""+fileNameString+"\" id=\""+fileNameString+"\">"+fileNameString+"</a>"+
          "\n\n\n"+
          "\n\t\t<div class=\"sig\">-J</div>"+
          "\n\t</article>"+
          "\n</div>";
-
-
-
-
-         myWriter.write(newHtmlString);
-         myWriter.close();
-         System.out.println("Successfully wrote to the file.");
+      String smallHtmlString = 
+      "<link rel=\"stylesheet\" media=\"screen\" href=\"https://fontlibrary.org/face/monoid\" type=\"text/css\"/>"+
+         "\n<div class=\"filterDiv show mb\">"+
+         "\n\t<article class=\"s\">" +
+         "\n\t\t<a class=\"date\" date=\""+fileNameString+"\" id=\""+fileNameString+"\">"+fileNameString+"</a>"+
+         "\n\n\n"+
+         "\n\t\t<div class=\"sig\">-J</div>"+
+         "\n\t</article>"+
+         "\n</div>";
+      try( FileWriter myWriter = new FileWriter(fileAndDirectoryString) ) 
+      {
+         //if incase for small or large post
+         if(checkForSubStringInFileName(fileNameString))
+         {
+            //its a microblog
+            myWriter.write(smallHtmlString);
+         } 
+         else 
+         {
+            //its a regular blog post
+            myWriter.write(largeHtmlString);
+         }
+         methodLogger.info("\nSuccessfully wrote to "+fileNameString+".\nFull path is "+fileAndDirectoryString);
       } catch (IOException e) {
-         System.out.println("An error occurred.");
+         methodLogger.info("\n!!!An error occurred.");
          e.printStackTrace();
-      }
+      } 
       
    }
-   /**Makes directory and returns the string
+
+   /**
+    * This method checks the fileNameString to see if the file to be created is a microblog post. 
+    * It then writes the appropriate text to the file after creation.
+    * @param fileNameString
+    * @return 
+    */
+   public static boolean checkForSubStringInFileName(String fileNameString ){
+      //check if mb is in fileNameString
+      Logger methodLogger = Logger.getLogger(makePost.class.getName()+"-m2");
+      methodLogger.info("\nChecking to see if this is a regular Blog post or a MicroBlog post.");
+      String substr = "mb";
+      int n1 = fileNameString.length();
+      int n2 = substr.length();
+      for (int i = 0; i <= n1 - n2; i++) 
+      {
+         int j;
+         for (j = 0; j < n2; j++) 
+         {
+            if (fileNameString.charAt(i + j) != substr.charAt(j))
+               break;
+         }
+         if (j == n2) 
+         {
+            methodLogger.info("\nThis is a MicroBlog post");
+            return true;
+         }
+      }
+      methodLogger.info("\nThis is a regular Blog post");
+      return false;
+   }
+
+   /**
+    * Makes directory and returns the string
     * 
     * @param fn
+    * @return
+    * @throws IOException
     */
-   public static String makeDirectoryWithFolderName(String fn){
+   public static String makeDirectoryWithFolderName(String fn) throws IOException {
       //get current directory 
-      System.out.println("Working Directory = " + System.getProperty("user.dir"));
-      String wd = System.getProperty("user.dir");
+      Logger methodLogger = Logger.getLogger(makePost.class.getName()+"-m3");
+      methodLogger.info("\nWorking Directory = " + System.getProperty("user.dir"));
+      String fd = System.getProperty("user.dir")+"\\posts\\"+fn;
       //make new sub directory
-      System.out.println("New Directory will be "+wd+"\\posts\\"+fn);
-      String fd = wd+"\\posts\\"+fn;
+      methodLogger.info("\nNew Directory will be "+fd);
       //check for directory
       File f = new File(fd);
+      String tempOutPut = "";
       if (f.exists() && f.isDirectory()) {
-         System.out.println("Directory aleady exists!");
-      } else 
+         methodLogger.info("\nDirectory aleady exists!\n<#######################################################>");
+         //if it already exists, show me whats inside
+         Files.list(new File(fd).toPath())
+                .limit(10)
+                .forEach(path -> {
+                  //tempOutPut += "\n"+path;
+                  methodLogger.info("-"+path);
+                });
+                
+         methodLogger.info("\n<#######################################################>");
+      } 
+      else 
       {
-         //System.out.println("Directory doesn't exist.  ");
          //make directory
          File theDir = new File(fd);
          if (!theDir.exists()){
             theDir.mkdirs();
-         }
+         }  
       }
-
-      
       return fd;
    }
 
    /**
-    * 
-
+    * Accepts file name as fn and the desired directory as nd.
+    * Combines them into a useable path.
+    * Returns an error if there was a problem, or the path string if all went according to plan.
+    * @param fn
+    * @param nd
+    * @return rs
     */
    public static String makeFileWithFileName(String fn,String nd){
-      
-      try {
-         File myObj = new File(nd+"\\"+fn+".html");
+      Logger methodLogger = Logger.getLogger(makePost.class.getName()+"-m4");
+      String rs = "";
+      try 
+      {
+         rs=nd+"\\"+fn+".html";
+         File myObj = new File(rs);
          if (myObj.createNewFile()) {
-           System.out.println("File created: " + myObj.getName());
+         methodLogger.info("\nFile created: " + myObj.getName());
+            return rs;
          } else {
-           System.out.println("File already exists.");
+            methodLogger.info("\n[!]File already exists!");
+            return nd+"\\"+fn+"_FILE_ALREADY_EXISTS.txt";
          }
-       } catch (IOException e) {
-         System.out.println("An error occurred.");
+      } 
+      catch (IOException e) 
+      {
+         methodLogger.info("\n[!]File could not be created. Exception e thrown!");
          e.printStackTrace();
-       }
-
-      return nd+"\\"+fn+".html";
-
+         return nd+"\\"+fn+"_FILE_COULD_NOT_BE_CREATED.txt";
+      }
    }
 
 
-   /**
+   /** TEST METHOD
     * 
     */
    public static void getFolderNameTESTMethod(){
@@ -166,117 +244,140 @@ public class makePost {
       
    }
 
-   /**getFolderNameTEST
-   *gets folder name from console input
-   */
-   public static String getFolderNameTEST(String kbd)
-   {
-      //Scanner kbd = new Scanner(System.in);
-      String folderName;
-
-      do
-      {
-         System.out.println("please enter a folder name");
-         //folderName = kbd.next();
-         folderName = kbd;
-         System.out.println("\n\n You typed: \""+folderName+"\"\n");
-         // if( getValidInput() ){
-         //    return folderName;
-         // } else {
-         //    System.out.println("Okay... Lets try again.");
-         // }
-
-         //skip the yes no for testing purposes
-         return folderName;
-      }while(true);
-
-   }
 
 
-   /**getFolderName
-   *gets folder name from console input
-   */
+   /**
+    * Gets the foldername with a scanner from keyboard input.  
+    * @return folderName
+    */
    public static String getFolderName()
    {
+      Logger methodLogger = Logger.getLogger(makePost.class.getName()+"-m5");
+      methodLogger.info("DEBUG MSG\n");
       Scanner kbd = new Scanner(System.in);
       String folderName;
- 
       do
       {
-         System.out.println("please enter a folder name");
+         System.out.println("\nPlease enter a folder name");
          folderName = kbd.next();
-         System.out.println("\n\n You typed: \""+folderName+"\"");
-         if( getValidInput() ){
-            return folderName;
-         } else {
-            System.out.println("Okay... Lets try again.");
+         System.out.println("\nYou typed: \""+folderName+"\"");
+         if( checkInputAgainstRegex(folderName) ){
+            if( getValidInput() ){
+               //kbd.close();
+               return folderName;
+            } else {
+               System.out.println("\n\nOkay... Lets try again.");
+            }
          }
       }while(true);
-
+      
    }
 
 
-   /**getFileName
-   *gets file name from console
-   */
+   /**
+    * Gets file name from user input using keyboard scenner
+    * @return fileName
+    */
    public static String getFileName()
    {
-      
+      Logger methodLogger = Logger.getLogger(makePost.class.getName()+"-m6");
+      methodLogger.info("DEBUG MSG\n");
       Scanner kbd = new Scanner(System.in);
       String fileName;
-     
       do
-      {
-         System.out.println("please enter a file name");
-         fileName = kbd.next();
+      {  
+         System.out.println("\nPlease enter a file name");
+         fileName = kbd.nextLine();
          System.out.println("\n\n You typed: \""+fileName+"\"");
-         if( getValidInput() ){
-            return fileName;
-         } else {
-            System.out.println("Okay... Lets try again.");
+         if( checkInputAgainstRegex(fileName) ){
+            if( getValidInput() ){
+               //kbd.close();
+               return fileName;
+            } else {
+               System.out.println("\n\nOkay... Lets try again.");
+            }
          }
+         
       }while(true);
 
    }
 
 
-   /*getValidInput
-   * checks if user types y / yes or n / no
-   **/
+   /**
+    * Validates users input
+    * @return true
+    */
    public static boolean getValidInput()
    {
-      System.out.println(" Is this correct?  (y/n)");
+      Logger methodLogger = Logger.getLogger(makePost.class.getName()+"-m7");
+      methodLogger.info("DEBUG MSG\n");
+      System.out.println("[!]Is this correct?  (y/n)");
       Scanner kbd = new Scanner(System.in);
-      
       String inp;
-     
       do
       {
          inp = kbd.next();
          if( inp.charAt(0) == 'y' || inp.equals("y") )
          {
-            
-            System.out.println("You typed y.");
-        
+            System.out.println("[!]You typed y.\n-------------------------");
             return true;
          }
          else if(  inp.charAt(0) =='n' || inp.equals("n") )
          {
-            System.out.println("You typed n.");
-        
+            System.out.println("[!]You typed n.\n-------------------------");
+            kbd.close();
             return false;
          } else 
          {
-            System.out.println("Invalid entry.\nYou typed ["+inp+"]\nPlease type y or n.");
-            
+            System.out.println("[!]Invalid entry.\nYou typed ["+inp+"]\nPlease type y or n.\n-------------------------");  
          }
-
       }
       while( true );
-      
-      
-      
-      
    }  
    
+
+   /**
+    * 
+    * @param userInputString
+    * @return matcher
+    */
+   public static boolean checkInputAgainstRegex(String userInputString){
+      Logger methodLogger = Logger.getLogger(makePost.class.getName()+"-m7");
+      methodLogger.info("DEBUG MSG\n");
+      Pattern pattern;
+      Matcher matcher;
+      String REGEX = "([0-9]{4}-[0-9]{2}-[0-9]{2})";
+      pattern = Pattern.compile(REGEX);
+      matcher = pattern.matcher(userInputString);
+      if(matcher.find()) { 
+         methodLogger.info("Input Valid");
+         return true;       
+      } else {
+         methodLogger.info("Input Invalid");
+         return false;
+      } 
+   }
+
+   /**
+    * 
+    * @param userInputString
+    * @return matcher
+    */
+    public static boolean checkInputAgainstRegexForMicroBlog(String userInputString){
+      Logger methodLogger = Logger.getLogger(makePost.class.getName()+"-m8");
+      methodLogger.info("DEBUG MSG\n");
+      Pattern pattern;
+      Matcher matcher;
+      String REGEX = "([0-9]{4}-[0-9]{2}-[0-9]{2}-[m][b]-[0-9])";
+      pattern = Pattern.compile(REGEX);
+      matcher = pattern.matcher(userInputString);
+      if(matcher.find()) { 
+         methodLogger.info("Input Valid");
+         return true;       
+      } else {
+         methodLogger.info("Input Invalid");
+         return false;
+      }  
+   }
+
 }
